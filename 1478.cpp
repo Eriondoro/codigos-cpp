983d9c313e971397cdd06e130f46bb422c7a8d9d
Eriondoro <eliandrosjunior@gmail.com>
2025-06-10 21:28:08 -0300

    ainda travado na 2869 mas subi mais meio degrau

--- 1478.cpp
+++ 1478.cpp
@@ -6,15 +6,16 @@ int main(){
 
     while(w != 0){
         cin >> w;
+        if(w == 0) break;
         for(i=0; i<w; i++){
             for(j=0; j<w; j++){
-                // abs faz 3-2 e 2-3 serem 1 "positivo"
                 cout << abs(i-j)+1;
                 if(j < w-1) cout << " ";
             }
             cout << endl;
         }
-        cout << endl;
+        cin.peek(); // Garante que não há leitura extra
+        if(cin.peek() != EOF) cout << endl;
     }
     
     return 0;
